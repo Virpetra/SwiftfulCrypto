710f355db4728ac7ed887c78a360035f9d47c155
Virpetra <msaiddede@gmail.com>
2024-02-01 20:48:23 +0300

    .

--- SwiftfulCrypto/Extensions/Double.swift
+++ SwiftfulCrypto/Extensions/Double.swift
@@ -9,7 +9,33 @@ import Foundation
 
 extension Double {
     
-    /// Converts a Double a Currency with 2-6 devimal places
+    /// Converts a Double a Currency with 2 decimal places
+    /// ```
+    /// Convert 1234.56 to $1,234.56
+    /// ```
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+      //  formatter.locale = .current // <- default value
+       // formatter.currencyCode = "usd"  // <- change currency
+       // formatter.currencySymbol = "$"  // <- change currency symbol
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    /// Converts a Double a Currency as a String with 2  decimal places
+    /// ```
+    /// Convert 1234.56 to "$1,234.56"
+    /// ```
+    
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
+    
+    /// Converts a Double a Currency with 2-6 decimal places
     /// ```
     /// Convert 1234.56 to $1,234.56
     /// Convert 12.3456 to $12,3456
@@ -27,7 +53,7 @@ extension Double {
         return formatter
     }
     
-    /// Converts a Double a Currency as a String with 2-6 devimal places
+    /// Converts a Double a Currency as a String with 2-6 decimal places
     /// ```
     /// Convert 1234.56 to "$1,234.56"
     /// Convert 12.3456 to "$12,3456"
