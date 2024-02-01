5551601c15b55c54fc111aaa75128ada7ad0b841
Virpetra <msaiddede@gmail.com>
2024-02-01 20:45:35 +0300

    Designed url

---
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  SwiftfulCrypto
+//
+//  Created by Mehmet Said Dede on 1.02.2024.
+//
+
+import Foundation
---
+++ SwiftfulCrypto/Services/CoinDataService.swift
@@ -0,0 +1,14 @@
+//
+//  CoinDataService.swift
+//  SwiftfulCrypto
+//
+//  Created by Mehmet Said Dede on 1.02.2024.
+//
+
+import Foundation
+
+class CoinDataService {
+    
+    @Published var allCoins: [CoinModel] = []
+    
+}
